c9c37b5fb8cc39eafc3ed72ff8d8303d3d18f3c4
infinitEnigma <yole71@gmail.com>
2020-04-11 01:12:53 +0200

    add custom-training-iris.py

---
+++ customization/custom-training-iris.py
@@ -0,0 +1,234 @@
+# This guide uses machine learning to categorize Iris flowers by species
+# This tutorial is structured like many TensorFlow programs:
+#   Import and parse the dataset.
+#   Select the type of model.
+#   Train the model.
+#   Evaluate the model's effectiveness.
+#   Use the trained model to make predictions.
+
+# Configure imports
+import os
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+print("TensorFlow version: {}".format(tf.__version__))
+print("Eager execution: {}".format(tf.executing_eagerly()))
+
+# The Iris classification problem
+# Imagine you are a botanist 
+# seeking an automated way to categorize each Iris flower you find
+# you're going to classify Iris flowers 
+# based on the length and width measurements of their sepals and petals.
+
+# Download the training dataset file using the tf.keras.utils.get_file function. 
+# This returns the file path of the downloaded file
+#train_dataset_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv"
+train_dataset_url = "file:///d:/github/repos/tensorflow-2/customization/data/iris_training.csv"
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url), origin=train_dataset_url)
+
+print("Local copy of the dataset file: {}".format(train_dataset_fp))
+
+# This dataset, iris_training.csv, 
+# is a plain text file that stores tabular data formatted as comma-separated values
+# column order in CSV file
+column_names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+
+feature_names = column_names[:-1]
+label_name = column_names[-1]
+
+print("Features: {}".format(feature_names))
+print("Label: {}".format(label_name))
+
+class_names = ['Iris setosa', 'Iris versicolor', 'Iris virginica']
+
+# Create a tf.data.Dataset
+batch_size = 32
+
+train_dataset = tf.data.experimental.make_csv_dataset(
+    train_dataset_fp,
+    batch_size,
+    column_names=column_names,
+    label_name=label_name,
+    num_epochs=1)
+
+features, labels = next(iter(train_dataset))
+
+print(features)
+
+# see some clusters by plotting a few features from the batch
+plt.scatter(features['petal_length'],
+            features['sepal_length'],
+            c=labels,
+            cmap='viridis')
+
+plt.xlabel("Petal length")
+plt.ylabel("Sepal length")
+plt.show()
+
+# To simplify the model building step, 
+# create a function to repackage the features dictionary 
+# into a single array with shape: (batch_size, num_features).
+def pack_features_vector(features, labels):
+  """Pack the features into a single array."""
+  features = tf.stack(list(features.values()), axis=1)
+  return features, labels
+
+train_dataset = train_dataset.map(pack_features_vector)
+
+features, labels = next(iter(train_dataset))
+
+print(features[:5])
+
+
+# Create a model using Keras
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(10, activation=tf.nn.relu, input_shape=(4,)),  # input shape required
+  tf.keras.layers.Dense(10, activation=tf.nn.relu),
+  tf.keras.layers.Dense(3)
+])
+
+# Let's have a quick look at what this model does to a batch of features
+predictions = model(features)
+predictions[:5]
+# To convert these logits to a probability for each class, use the softmax function:
+tf.nn.softmax(predictions[:5])
+
+print("Prediction: {}".format(tf.argmax(predictions, axis=1)))
+print("    Labels: {}".format(labels))
+
+
+# Train the model
+# 
+# Define the loss and gradient function
+loss_object = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+def loss(model, x, y, training):
+  # training=training is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  y_ = model(x, training=training)
+
+  return loss_object(y_true=y, y_pred=y_)
+
+
+l = loss(model, features, labels, training=False)
+print("Loss test: {}".format(l))
+
+# Use the tf.GradientTape context to calculate the gradients used to optimize your model:
+def grad(model, inputs, targets):
+  with tf.GradientTape() as tape:
+    loss_value = loss(model, inputs, targets, training=True)
+  return loss_value, tape.gradient(loss_value, model.trainable_variables)
+
+# Create an optimizer
+optimizer = tf.keras.optimizers.SGD(learning_rate=0.01)
+# We'll use this to calculate a single optimization step:
+loss_value, grads = grad(model, features, labels)
+
+print("Step: {}, Initial Loss: {}".format(optimizer.iterations.numpy(),
+                                          loss_value.numpy()))
+
+optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+print("Step: {},         Loss: {}".format(optimizer.iterations.numpy(),
+                                          loss(model, features, labels, training=True).numpy()))
+
+
+# Training loop
+## Note: Rerunning this cell uses the same model variables
+
+# Keep results for plotting
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs = 201
+
+for epoch in range(num_epochs):
+  epoch_loss_avg = tf.keras.metrics.Mean()
+  epoch_accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+
+  # Training loop - using batches of 32
+  for x, y in train_dataset:
+    # Optimize the model
+    loss_value, grads = grad(model, x, y)
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+    # Track progress
+    epoch_loss_avg.update_state(loss_value)  # Add current batch loss
+    # Compare predicted label to actual label
+    # training=True is needed only if there are layers with different
+    # behavior during training versus inference (e.g. Dropout).
+    epoch_accuracy.update_state(y, model(x, training=True))
+
+  # End epoch
+  train_loss_results.append(epoch_loss_avg.result())
+  train_accuracy_results.append(epoch_accuracy.result())
+
+  if epoch % 50 == 0:
+    print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+                                                                epoch_loss_avg.result(),
+                                                                epoch_accuracy.result()))
+
+
+# Visualize the loss function over time
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+fig.suptitle('Training Metrics')
+
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+plt.show()
+
+
+# Evaluate the model's effectiveness
+# Now that the model is trained, we can get some statistics on its performance.
+#test_url = "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv"
+test_url = "file:///d:/github/repos/tensorflow-2/customization/data/iris_test.csv"
+test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                  origin=test_url)
+
+test_dataset = tf.data.experimental.make_csv_dataset(
+    test_fp,
+    batch_size,
+    column_names=column_names,
+    label_name='species',
+    num_epochs=1,
+    shuffle=False)
+
+test_dataset = test_dataset.map(pack_features_vector)
+
+
+# Evaluate the model on the test dataset
+test_accuracy = tf.keras.metrics.Accuracy()
+
+for (x, y) in test_dataset:
+  # training=False is needed only if there are layers with different
+  # behavior during training versus inference (e.g. Dropout).
+  logits = model(x, training=False)
+  prediction = tf.argmax(logits, axis=1, output_type=tf.int32)
+  test_accuracy(prediction, y)
+
+print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+
+# We can see on the last batch, for example, the model is usually correct:
+tf.stack([y,prediction],axis=1)
+
+
+# Use the trained model to make predictions
+predict_dataset = tf.convert_to_tensor([
+    [5.1, 3.3, 1.7, 0.5,],
+    [5.9, 3.0, 4.2, 1.5,],
+    [6.9, 3.1, 5.4, 2.1]
+])
+
+# training=False is needed only if there are layers with different
+# behavior during training versus inference (e.g. Dropout).
+predictions = model(predict_dataset, training=False)
+
+for i, logits in enumerate(predictions):
+  class_idx = tf.argmax(logits).numpy()
+  p = tf.nn.softmax(logits)[class_idx]
+  name = class_names[class_idx]
+  print("Example {} prediction: {} ({:4.1f}%)".format(i, name, 100*p))
+
